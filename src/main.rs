dca8e79d9f051e4cb656febbe1974f21721739cb
Tobias Kleine <tobias.kleine@gmx.net>
2024-10-17 07:17:18 +0000

    check in base of project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/* 
+ * First iteration: function to identify 5 common patterns in strings
+ * Second itegration: function to read a file and check for patterns
+ * Third iteration: CLI to real all files in a directory and check for patterns
+ * ...
+*/
