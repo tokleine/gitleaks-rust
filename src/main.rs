cd3671f853fadb3d36051b3d01affde4dbf4be36
Tobias Kleine <tobias.kleine@gmx.net>
2024-10-17 14:16:44 +0000

    simple single file checking without line numbers

--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,43 @@ use regex::Regex;
 use std::time::Instant;
 
 fn main() {
-    let re = [
-        (r"abc", "abc123"),
-        (
-            r"https?://.+:.+@dev.azure.com.*",
-            "https://username:password@dev.azure.com",
-        ),
-        (
-            r"https?://.+:.+@dev.azure.com.*",
-            "https://username.password@dev.azure.com",
-        ),
-        (
-            r"eyj[a-zA-Z0-9\-_%]+\.eyj[a-zA-Z0-9\-_%]+\.[a-zA-Z0-9\-_%]+", // OAuth JWT
-            "eyj0eXAiOiJKV1.eyjhdWQiOMi4wIn0.OnvSu-8w",
-        ),
-        (r"token", "asd the token is: eyj...."),
+    let patterns = [
+        (1,r"abc"),
+        (2,r"https?://.+:.+@dev.azure.com.*"),
+        (3,r"https?://.+:.+@dev.azure.com.*"),
+        (4,r"eyj[a-zA-Z0-9\-_%]+\.eyj[a-zA-Z0-9\-_%]+\.[a-zA-Z0-9\-_%]+"), // OAuth JWT
+        (5,r"[tT]oken")
     ];
 
-    let start = Instant::now();
-    let examples: Vec<_> = re
+    let regexes: Vec<_> = patterns
         .iter()
-        .map(|(re, text)| (Regex::new(re).expect("Invalid regex string"), text))
+        .map(|(id,re)| (id,Regex::new(re).expect("Invalid regex string")))
         .collect();
-    println!("Time to compile regex: {:?}", start.elapsed());
 
+    // read file 
     let start = Instant::now();
-    for (re, text) in examples.iter() {
-        println!(
-            "Pattern: {:?}, Text: {:?}, Result: {:?}",
-            re,
-            text,
-            match_pattern_in_haystack(re, text)
-        );
-    }
-    println!("Time to match regex: {:?}", start.elapsed());
-}
+    let file_content = std::fs::read_to_string("data/test_file.txt").expect("Unable to read file");
+    println!("Reading file took: {:?}", start.elapsed());
 
-fn match_pattern_in_haystack(pattern: &Regex, haystack: &str) -> bool {
-    pattern.is_match(haystack)
+    // check for patterns
+    let start = Instant::now();
+    for (id, regex) in regexes.iter() {
+        if regex.is_match(&file_content) {
+            // let found_match = regex.find(&file_content).unwrap(); // limitation: only first match, no surrounding text
+            let found_matches = regex.find_iter(&file_content).map(|m| m.as_str()).collect::<Vec<&str>>(); // get all matches
+            println!("Found pattern '{}' {} times: {:?}", id, found_matches.len(), found_matches);
+        }
+    }
+    println!("Checking for patterns took: {:?}", start.elapsed());
+
 }
 
 /*
  * First iteration: function to identify 5 common patterns in strings - check
- * Second itegration: function to read a file and check for patterns
- * Third iteration: CLI to real all files in a directory and check for patterns
- * ...
+ * Second itegration: function to read a file and check for patterns, get line number and line text
+ * Third iteration: build CLI to read all files in a directory and check for patterns
+ * Fourth iteration: CLI to read all files in a directory and check for patterns, with exclusion list
+ * Fifth iteration: check git log for patterns
  *
  * Feature list:
  * regex pattern matching
