5fb74c89480c8f03ad75fb97efc751fc7d69d60e
Tobias Kleine <tobias.kleine@gmx.net>
2024-10-17 07:39:06 +0000

    basic pattern matching function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let re = r"abc";
+    let text = "ab123";
+    let result: bool = match_pattern_in_haystack(re, text);
+    println!("Result: {}", result);
 }
 
-/* 
+fn match_pattern_in_haystack(pattern: &str, haystack: &str) -> bool {
+    let re = Regex::new(pattern).unwrap();
+    re.is_match(haystack)
+}
+
+/*
  * First iteration: function to identify 5 common patterns in strings
  * Second itegration: function to read a file and check for patterns
  * Third iteration: CLI to real all files in a directory and check for patterns
  * ...
+ *
+ * Feature list:
+ * regex pattern matching
+ * false positive exlusion
+ * filepath & pre-fix exclusion
+ *
 */
